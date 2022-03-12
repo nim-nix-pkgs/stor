fc1e974203bb44f468273c307d703c8fa16d78d4
hugosenari <hugosenari@gmail.com>
2022-03-12 15:27:40 -0300

    chore: re index stor

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Efficient object storage system'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-stor-master.flake = false;
+  inputs.src-stor-master.owner = "nimscale";
+  inputs.src-stor-master.ref   = "refs/heads/master";
+  inputs.src-stor-master.repo  = "stor";
+  inputs.src-stor-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-stor-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-stor-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
